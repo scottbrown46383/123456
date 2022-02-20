c2dc1a056214118fabfb2c6a36f06f67dcb86dc9
scottbrown46383 <73317053+scottbrown46383@users.noreply.github.com>
2022-02-20 11:43:09 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+control.waitForEvent(0, 0)
+basic.showNumber(0)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
+let item: number
+basic.forever(function () {
+	
+})
